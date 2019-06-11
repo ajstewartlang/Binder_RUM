4caa5e2d1631678dfa79621a798c13479cf4105c
ajstewartlang <andrew.stewart@manchester.ac.uk>
2019-06-11 21:18:51 +0100

    Update script_model.R

--- script_model.R
+++ script_model.R
@@ -1,6 +1,6 @@
-# avoiding loading any additional packages - we're going base R all the way!
+# In this script we're avoiding loading any additional packages - we're going base R all the way!
 
-# Let's examine where 
+# Let's examine whether fuel economy is predicted by horsepower
 plot(mtcars$hp, mtcars$mpg, main = "MPG vs. Horsepower for mtcars\ndataset", xlab = "Horsepower", ylab = "MPG")
 abline(lm(mtcars$mpg ~ mtcars$hp), col = "blue")
 
